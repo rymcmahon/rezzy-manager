459472250156d28b50a7ff02456c25c324aa6f4c
rymcmahon <toedragdangle9@gmail.com>
2017-02-28 09:13:24 -0600

    added search params to table controller

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -4,7 +4,6 @@ class TablesController < ApplicationController
     @reserved_tables = Table.reserved_on(params[:guests], params[:day], params[:time])
     @open_tables = Table.free_on(params[:guests], params[:day], params[:time])
 
-    @guests = params[:guests]
     @day = params[:day]
     @time = params[:time]
   end
