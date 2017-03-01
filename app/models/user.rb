2fa73c7962c2b68c2c245ceca88d10c892021a63
rymcmahon <toedragdangle9@gmail.com>
2017-03-01 10:44:12 -0600

    added after save callback for updating reservation ends at

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   has_many :tables, through: :reservations
   accepts_nested_attributes_for :reservations, allow_destroy: true
   accepts_nested_attributes_for :tables, allow_destroy: true
+
+  after_save :update_ends_at
+
+  private
+    def update_ends_at
+      self.reservations.update(ends_at: self.reservations.last.starts_at.advance(hours: 1))
+    end
 end
