1747b0d2a608cdca2d2ed8d152e6e7246ef7d6f1
rymcmahon <toedragdangle9@gmail.com>
2017-03-03 13:33:59 -0600

    built out calendar with previous and next links

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module Reservations
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.beginning_of_week = :sunday
   end
 end
