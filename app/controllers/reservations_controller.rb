c26dc7683f4a42410ee04af0366671ab0aa957f4
rymcmahon <toedragdangle9@gmail.com>
2017-03-05 21:28:25 -0600

    added booked action and route and table of reservations by date

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,4 +2,9 @@ class ReservationsController < ApplicationController
   def index
     @reservations = Reservation.all
   end
+
+  def booked
+    @reservation_date = params[:day]
+    @reservations = Reservation.where(:date => @reservation_date)
+  end
 end
